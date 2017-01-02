1cca055dfdbb5fe0206847b77ba560e407456510
savybrandt <savybrandt@gmail.com>
2017-01-02 15:50:57 -0800

    Making progress through app.js methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,89 @@
 // YOUR CODE HERE:
+var app = {};
+
+app.init = function() {
+
+};
+
+app.send = function(message) {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: undefined,
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+
+  // $.ajax({
+  //   // This is the url you should use to communicate with the parse API server.
+  //   url: undefined,
+  //   type: 'DELETE',
+  //   data: $('#chats'),
+  //   contentType: 'application/json',
+  //   success: function (data) {
+  //     console.log('chatterbox: Message sent');
+  //   },
+  //   error: function (data) {
+  //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+  //     console.error('chatterbox: Failed to send message', data);
+  //   }
+  // });
+
+  $('#chats').html('');
+};
+
+app.renderMessage = function(message) {
+  //add message to #chats
+  $('#chats').append('<p>' + message + '</p>');
+};
+
+app.renderRoom = function(roomName) {
+  $('#roomSelect').append('<div>' + roomName + '</div>');        
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
